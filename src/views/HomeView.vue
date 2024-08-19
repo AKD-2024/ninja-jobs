83ecd1af44899695524ba5ca729fc9b42a5556d0
Anjali <anjali.dubey@comprotechnologies.com>
2024-08-19 18:16:02 +0530

    Vue: Training

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius similique ab nisi molestiae itaque eaque earum officiis nemo necessitatibus beatae libero quas nam ad sapiente quis tempora, aperiam minus laudantium veniam corrupti nulla nostrum. Optio possimus laboriosam repellat? Asperiores neque labore veritatis, cupiditate quae dolor! Laudantium expedita voluptatibus inventore quia reprehenderit. Voluptate mollitia eaque porro vel soluta, distinctio doloremque aliquam, eum esse animi sit eligendi! Aliquam, impedit id! Deserunt voluptatem illum impedit incidunt officia, neque fugit odit error aspernatur nemo pariatur quibusdam ad iure atque a expedita rerum possimus voluptatum! Molestias facere quam corrupti repellat et iste amet quaerat eveniet?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius similique ab nisi molestiae itaque eaque earum officiis nemo necessitatibus beatae libero quas nam ad sapiente quis tempora, aperiam minus laudantium veniam corrupti nulla nostrum. Optio possimus laboriosam repellat? Asperiores neque labore veritatis, cupiditate quae dolor! Laudantium expedita voluptatibus inventore quia reprehenderit. Voluptate mollitia eaque porro vel soluta, distinctio doloremque aliquam, eum esse animi sit eligendi! Aliquam, impedit id! Deserunt voluptatem illum impedit incidunt officia, neque fugit odit error aspernatur nemo pariatur quibusdam ad iure atque a expedita rerum possimus voluptatum! Molestias facere quam corrupti repellat et iste amet quaerat eveniet?</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
